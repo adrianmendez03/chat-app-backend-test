9cd1681c54d608643b3f04c19cb7523e2063a9b7
Adrian Mendez <adrianmndz328@gmail.com>
2021-02-26 14:26:18 -0600

    Socket.io bolierplate

--- server.js
+++ server.js
@@ -4,12 +4,24 @@ const { PORT = 3000, NODE_ENV = "development"} = process.env
 const cors = require('cors')
 const express = require('express')
 const app = express()
+const io = require('socket.io')(app)
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.json({ msg: "Default route... Nothing to see here" })
+    res.json(__dirname + '/index.html')
+})
+
+io.on('connection', socket => {
+    socket.broadcast.emit('joined', 'User has joined.')
+    socket.on('disconnected', () => {
+        io.emit('left', 'User has left')
+        console.log('User disconnected')
+    })
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg)
+    })
 })
 
 app.listen(PORT, () => {
