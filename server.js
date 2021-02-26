737d4b4ee99699073f691fe447d2458978e0298a
Adrian Mendez <adrianmndz328@gmail.com>
2021-02-26 14:38:06 -0600

    requried http

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const { PORT = 3000, NODE_ENV = "development"} = process.env
 const cors = require('cors')
 const express = require('express')
 const app = express()
-const io = require('socket.io')(app)
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
 app.use(cors())
 app.use(express.json())
